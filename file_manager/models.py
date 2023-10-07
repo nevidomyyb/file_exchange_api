166b0872b4ed7726780331ff93a013745e2a9d20
Pedro Cunha <idk.pedroc@gmail.com>
2023-10-07 01:24:47 -0300

    add: define serializer & fix: model

--- file_manager/models.py
+++ file_manager/models.py
@@ -7,9 +7,9 @@ import os
 class FileExchange(models.Model):
     id = models.AutoField(primary_key=True)
     file_path = models.FileField(upload_to='files/%Y/%m/%d/', blank=False, null=False)
-    code = models.CharField(max_length=128, null=False, blank=False)
+    code = models.CharField(max_length=10, null=False, blank=False)
     creation_at = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         db_table = "file_exchange"
 
---
+++ file_manager/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import FileExchange
+import os
+import random
+from django.core.exceptions import ValidationError
+from rest_framework.serializers import RelatedField
+from string import ascii_letters, digits
+
+class CreateCodeField(RelatedField):
+
+    def to_representation(self, value):
+        if value: raise ValidationError('Please do not send a code value')
+        valid_digits = [*ascii_letters, *digits]
+        code_len = random.randint(0, 9)
+        code = ""
+        for i in range(0, code_len):
+            rand_value = random.randint(0,len(valid_digits))
+            code+=valid_digits[rand_value]
+        value = code
+        return code
+
+def validate_file_extensions(value):
+    extension = os.path.splitext(value.name)[1].lower()
+    valid_extensions =  ['.pdf', '.xls', '.xlsx', '.rar', '.jpg', '.jpeg', '.png', '.gif']
+    if not extension in valid_extensions:
+        raise ValidationError('Unsupported file extension')
+
+class FileManagerSerializer(serializers.ModelSerializer):
+
+    id = serializers.IntegerField(required=False, read_only=True)
+    file_path = serializers.FileField()
+    code = CreateCodeField(required=False, read_only=True)
+    creation_at = serializers.DateTimeField(required=False, read_only=True)
+
+    class Meta:
+        model = FileExchange
+        fields = ['id', 'file_path', 'code', 'creation_at']
+    
+    def validate_file_path(self, value):
+        validate_file_extensions(value)
+        return value
+
+    def validate_code(self, value):
+        if value: raise ValidationError('Please do not send a code value')
+        return value
+        
